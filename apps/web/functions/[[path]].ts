c0917c24cbafb6557c15d4a2f67b5563133fe245
slichti <slichti@gmail.com>
2026-01-03 21:33:04 -0500

    fix: resolve cloudflare pages 404 with proper compilation config

--- apps/web/functions/[[path]].ts
+++ apps/web/functions/[[path]].ts
@@ -1,5 +1,5 @@
 import { createRequestHandler } from "@react-router/cloudflare";
 // @ts-ignore - The build file is generated at build time
-import * as build from "../build/server/index.js";
+import * as build from "./_server.js";
 
 export const onRequest = createRequestHandler({ build });
