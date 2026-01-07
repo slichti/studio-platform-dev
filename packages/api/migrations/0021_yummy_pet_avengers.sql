5e78d80a5a4578d6b558d1e998795398b7e26f97
slichti <slichti@gmail.com>
2026-01-06 19:57:00 -0500

    fix: Add default value for trigger_event migration

--- packages/api/migrations/0021_yummy_pet_avengers.sql
+++ packages/api/migrations/0021_yummy_pet_avengers.sql
@@ -1,5 +1,5 @@
 DROP INDEX `automation_tenant_trigger_idx`;--> statement-breakpoint
-ALTER TABLE `marketing_automations` ADD `trigger_event` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `marketing_automations` ADD `trigger_event` text DEFAULT 'new_student' NOT NULL;--> statement-breakpoint
 ALTER TABLE `marketing_automations` ADD `trigger_condition` text;--> statement-breakpoint
 ALTER TABLE `marketing_automations` ADD `timing_type` text DEFAULT 'immediate' NOT NULL;--> statement-breakpoint
 ALTER TABLE `marketing_automations` ADD `timing_value` integer DEFAULT 0;--> statement-breakpoint
