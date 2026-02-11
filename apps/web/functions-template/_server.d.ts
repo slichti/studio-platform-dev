0f78c324ac9bf1ba258c4a610a9598f12705699e
slichti <slichti@gmail.com>
2026-02-10 22:39:17 -0500

    fix(web): add _server.d.ts to satisfy typecheck

---
+++ apps/web/functions-template/_server.d.ts
@@ -0,0 +1,12 @@
+
+declare const assets: any;
+declare const assetsBuildDirectory: any;
+declare const basename: any;
+declare const entry: any;
+declare const future: any;
+declare const isSpaMode: any;
+declare const publicPath: any;
+declare const routes: any;
+declare const ssr: any;
+
+export { assets, assetsBuildDirectory, basename, entry, future, isSpaMode, publicPath, routes, ssr };
