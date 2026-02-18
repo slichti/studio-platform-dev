e323a05fc575544211e32f461e70a4aebed4ee60
slichti <slichti@gmail.com>
2026-02-17 21:52:35 -0500

    Fix 405 Method Not Allowed on settings revalidation

--- apps/web/app/routes/studio.$slug.settings.tsx
+++ apps/web/app/routes/studio.$slug.settings.tsx
@@ -1,4 +1,9 @@
 import { Outlet, useOutletContext } from "react-router";
+import type { ActionFunction } from "react-router";
+
+export const action: ActionFunction = async () => {
+    return null;
+};
 
 export default function SettingsLayout() {
     const context = useOutletContext();
