6758c8f682c543a37a4a8df734426de31e69ed5c
slichti <slichti@gmail.com>
2026-03-10 20:31:08 -0400

    fix(web): resolve useLoaderData crash in react-router v7 by fixing dual-package hazard in entry files

--- apps/web/app/entry.server.tsx
+++ apps/web/app/entry.server.tsx
@@ -1,7 +1,7 @@
 
 import type { AppLoadContext, EntryContext } from "react-router";
 
-import { ServerRouter } from "react-router-dom";
+import { ServerRouter } from "react-router";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
 
